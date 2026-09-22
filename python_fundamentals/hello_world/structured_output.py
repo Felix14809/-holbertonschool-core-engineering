b4aa17a737d610a649cac04a1d6c1c4ca73fb303
Felix Harding <j.felixharding@gmail.com>
2026-09-22 10:54:48 +1000

    first task

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import math
+
+language = "Language: Python"
+x = 1
+
+print (language)
+print("Version: 3")
+print("Pi approx: %.2f" % math.pi)
+if (x == 1):
+	print("Computation valid: True")
