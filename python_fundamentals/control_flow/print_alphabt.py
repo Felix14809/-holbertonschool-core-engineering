dbb26e55a6340863465c74b36aa1fd0b5fcba4fa
Felix Harding <j.felixharding@gmail.com>
2026-09-23 17:19:59 +1000

    fixes

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 letter = 97
-for i in range(25):
+for i in range(26):
     if chr(letter) != 'e' and chr(letter) != 'q':
         print("{0}".format(chr(letter)), end='')
     letter = letter + 1
