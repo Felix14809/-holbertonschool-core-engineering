392682a511720b90a67c387108ac449afb7befdb
Felix Harding <j.felixharding@gmail.com>
2026-09-23 17:01:39 +1000

    format

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -4,7 +4,7 @@ if n > 0 and n % 10 > 5:
     print(f"Last digit of {n} is {n % 10} and is greater than 5")
 elif n % 10 == 0:
     print("Last digit of {0} is 0 and is 0".format(n))
-elif n % 10 < 6:
+elif n < 0 or n % 10 < 6:
     if n < 0:
         i = (n * -1) % 10 * -1
         print(f"Last digit of {n} is {i} and is less than 6 and not 0")
